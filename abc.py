9bd9728a8c3621ea7133637ff849c28fe6e7b95f
Aakanksha Saini <aakanksha.saini595@gmail.com>
2026-02-03 22:15:45 +0530

    final

--- abc.py
+++ abc.py
@@ -1,25 +1,15 @@
-# #!/bin/python3
-import math
-import os
-import random
-import re
 import sys
-# # Complete the 'fizzBuzz' function below.
-# #
-# # The function accepts INTEGER n as parameter.
-# #
-
 def fizzBuzz(n):
     # Write your code here
     for i in range (1,n+1):
         if(i%3 ==0 and i%5 ==0):
-            print("Fizzbuzz");
+            print("Fizzbuzz")
         elif( i % 3 == 0):
             print("fuzz")
         elif(i%5==0):
             print("buzz")
         else:
-            print(i);
+            print(i)
                         
 
 if __name__ == '__main__':
